62e301cedf0000c53c6cad8480943202223a3593
ClementVaugoyeau <clement.vaugoyeau@gmail.com>
2023-09-09 14:48:45 +0200

    Initial commit

---
+++ src/test/java/com/example/ecommerceback/ECommerceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
